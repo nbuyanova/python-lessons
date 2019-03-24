9a7f74b7e686e8b5c392c905436232b479151040
ns.buyanova <ns.buyanova@gmail.com>
2019-03-24 20:03:14 +0300

    edit

--- vsearch.py
+++ vsearch.py
@@ -1,5 +1,9 @@
-def search4vowels(word):
-    """Выводит гласные, найденные в указанном слове."""
+def search4vowels(phrase: str) -> set:
+    """Возвращает гласные, найденные в указанной фразе."""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    return bool(found)
+    return vowels.intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
+    """Возвращает множество букв из 'letters', найденных в указанной фразе."""
+    return set(letters).intersection(set(phrase))
